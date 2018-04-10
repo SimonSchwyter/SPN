c676553ffa708ce1bf0b2788064cbcc6a9138d67
Simon Schwyter <simon.schwyter1@swisscom.com>
2018-04-10 16:37:33 +0200

    added sbox

--- src/Spn.java
+++ src/Spn.java
@@ -1,20 +1,36 @@
 public class Spn {
     private String text ="";
     private final int[][] bitpermut = new int[][]{
-        {0,1,2,3,4},{4,3,2,1,0}
+        {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15},{0,4,8,12,1,5,9,13,2,6,10,14,3,7,11,15}
     };
+    private String[][] sBox;
     private String[] keys;
-    public Spn(String text, String baseKey, int keyCount) {
+    public Spn(String text, String baseKey, int keyCount,String[][] sBox) {
         this.text = text;
         this.keys = calcKeys(baseKey, keyCount);
-
+        this.sBox = sBox;
     }
 
     public String encript(){
-        /*for(int i =0; i < this.keys.length(); i++){
+        //this.keys = invertKeys();
+        String encriptedText="";
+        this.text = runSBox();
+        System.out.println(this.text);
+        for(int i =0; i < this.keys.length; i++){
 
-        }*/
-        return "";
+        }
+        return encriptedText;
+    }
+
+    private String[] invertKeys() {
+        String[] newKeys = new String[this.keys.length];
+        for(int i =0; i<this.keys.length; i++){
+            if(i != 0 && i != this.keys.length-1){
+                this.keys[this.keys.length-i] = runBitpermutation(this.keys[this.keys.length-i]);
+            }
+            newKeys[i] = this.keys[this.keys.length-i];
+        }
+        return newKeys;
     }
 
     /**Calculate keys from basekey
@@ -35,7 +51,6 @@ public class Spn {
                  item ++;
             }
             keys[i]=key;
-            System.out.println(keys[i]);
             block++;
         }
         return keys;
@@ -47,13 +62,22 @@ public class Spn {
 
 
     private String runSBox(){
-        return "";
+        String newText="";
+        String[] textBlocks = this.text.split("(?<=\\G....)");
+        for(int i=0; i < textBlocks.length;i++){
+            for(int j=0; j < textBlocks.length;j++){
+                if(textBlocks[i].equals(this.sBox[0][j])){
+                    newText += this.sBox[1][j];
+                }
+            }
+        }
+        return newText;
     }
-    private String runSBoxInverted(){return "";}
-    private String runBitpermutation(){
+
+    private String runBitpermutation(String text){
         String newText= "";
-        for(int i=0; i < this.text.length(); i++){
-            newText += this.text.toCharArray()[bitpermut[1][i]];
+        for(int i=0; i < text.toCharArray().length; i++){
+            newText += text.toCharArray()[bitpermut[1][i]];
         }
         return newText;
     }
