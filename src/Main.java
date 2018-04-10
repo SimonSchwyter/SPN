9416fbe6d15c9b09c9f25950cf56929bbe50f83a
Simon Schwyter <simon.schwyter1@swisscom.com>
2018-04-10 15:28:47 +0200

    added key calc

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-Spn myCrypteText = new Spn("Simon");
+Spn myCrypteText = new Spn("Simon", "00111010100101001101011000111111", 4);
     }
 }
--- src/Spn.java
+++ src/Spn.java
@@ -3,9 +3,42 @@ public class Spn {
     private final int[][] bitpermut = new int[][]{
         {0,1,2,3,4},{4,3,2,1,0}
     };
-
-    public Spn(String text) {
+    private String[] keys;
+    public Spn(String text, String baseKey, int keyCount) {
         this.text = text;
+        this.keys = calcKeys(baseKey, keyCount);
+
+    }
+
+    public String encript(){
+        /*for(int i =0; i < this.keys.length(); i++){
+
+        }*/
+        return "";
+    }
+
+    /**Calculate keys from basekey
+     * @params String basekey
+     * @return String Array of keys k0, k1, ..., kn-1 */
+    private String[] calcKeys(String keybase, int keyCount){
+        String[] keys = new String[keyCount+1];
+        int block =0;
+        for(int i =0; i< keyCount+1; i++){
+            String key = "";
+            int item =0;
+            for(int j=0; j<16; j++){
+                if(4*block+j == keybase.toCharArray().length){
+                block=0;
+                item =0;
+                }
+                 key += keybase.toCharArray()[4*block+item];
+                 item ++;
+            }
+            keys[i]=key;
+            System.out.println(keys[i]);
+            block++;
+        }
+        return keys;
     }
 
     public String getText() {
@@ -17,7 +50,7 @@ public class Spn {
         return "";
     }
     private String runSBoxInverted(){return "";}
-    public String runBitpermutation(){
+    private String runBitpermutation(){
         String newText= "";
         for(int i=0; i < this.text.length(); i++){
             newText += this.text.toCharArray()[bitpermut[1][i]];
